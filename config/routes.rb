e36c63b60bb5c388cee2d7bbc0979b97c1930ffc
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-05-24 06:12:53 +0000

    added .env

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get("/backdoor",{:controller =>"items",:action =>"new_item_form"})
 
   post("/insert_item",{:controller => "items",:action=>"create_new_item"})
+
+  get("/rake_tasks", { :controller => "rake_tasks", :action => "show" })
+  get("/run_task", { :controller => "rake_tasks", :action => "run_task" })
 end
